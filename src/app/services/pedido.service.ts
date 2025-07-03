b941338c10467576ea911ce337a8a4edd6d90d2d
manubernardi <manuber29@gmail.com>
2025-07-03 15:44:45 -0300

    Listar pedidos anda boton volver no

--- src/app/services/pedido.service.ts
+++ src/app/services/pedido.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Pedido } from '../pages/list-order/list-order.component'
+import { HttpParams } from '@angular/common/http';
 
 
 @Injectable({ providedIn: 'root' })
@@ -10,7 +11,18 @@ export class PedidoService {
 
   constructor(private http: HttpClient) {}
 
-  getPedidos(): Observable<{ items: Pedido[] }> {
+  getPedidos(page: number = 1, limit: number = 10): Observable<any> {
+  const params = new HttpParams()
+    .set('page', page.toString())
+    .set('limit', limit.toString());
+
+  return this.http.get<any>(this.apiUrl, { params });
+}
+}
+  /*getPedidos(): Observable<{ items: Pedido[] }> {
     return this.http.get<{ items: Pedido[] }>(this.apiUrl);
   }
-}
+}/*
+/*getPedidos(page: number = 1, limit: number = 10): Observable<any> {
+  return this.http.get<any>(`/pedidos?page=${page}&limit=${limit}`);
+}*/
