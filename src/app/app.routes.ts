9a1fc06c48246f27a5e1885a7083e4fc2202da32
augustosan-182 <augustosan7@gmail.com>
2025-08-07 12:41:21 -0300

    opcion Mi Carrito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -43,6 +43,11 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/perfil/perfil.component').then(m => m.PerfilComponent),
     canActivate: [authGuard]
   },
+  {
+    path: 'carrito',
+    loadComponent: () => import('./pages/create-order/create-order').then(m => m.CreateOrder),
+    canActivate: [authGuard]
+  },
   
 
   //rutas 
