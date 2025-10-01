6972942d82abf55f19a33eb4c232662b24213340
manubernardi <manuber29@gmail.com>
2025-10-01 11:37:50 -0300

    Renuevo api

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,6 +1,6 @@
 export const config = {
   urls: {
-    getFood: 'https://mocki.io/v1/bf62ee8c-ca16-432e-b1a4-6be9d620db4e',
+    getFood: 'https://mocki.io/v1/224b241f-4bb0-4acc-aedb-9781bf6513f5',
     getCart: 'https://mocki.io/v1/def365b4-88e6-40a1-bbb1-f281cc6adc21',
   },
 };
